928ef39088867235493f0970d1c2e70f6879cd99
Sean McLellan <Oceanswave@users.noreply.github.com>
2022-08-23 13:42:05 -0400

    Update train.ps1

--- train.ps1
+++ train.ps1
@@ -4,14 +4,14 @@ $env:PL_TORCH_DISTRIBUTED_BACKEND="gloo"
 # python ./main.py --base ./configs/stable-diffusion/v1-finetune.yaml `
 #                                             -t `
 #                                             --actual_resume ../stable-diffusion-dream/models/ldm/stable-diffusion-v1/model.ckpt `
-#                                             -n isla_fisher `
+#                                             -n my_cat`
 #                                             --gpus 1 `
-#                                             --data_root D:/textual-inversion/isla_fisher `
-#                                             --init_Word 'Isla Fisher'
+#                                             --data_root D:/textual-inversion/my_cat `
+#                                             --init_Word 'cat'
 python ./main.py --base ./configs/stable-diffusion/v1-finetune.yaml `
                                             -t `
                                             --actual_resume ../stable-diffusion-dream/models/ldm/stable-diffusion-v1/model.ckpt `
                                             --gpus 1 `
                                             --data_root D:/textual-inversion/isla_fisher `
-                                            --resume 'logs/isla_fisher2022-08-23T01-46-37_isla_fisher' `
-                                            --init_Word 'Isla Fisher'
+                                            --resume 'logs/my_cat2022-08-23T01-46-37_my_cat' `
+                                            --init_Word 'cat'
