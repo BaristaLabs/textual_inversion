a2181e0964dacb9cac9eacf73b50f875389c96bf
Sean McLellan <Oceanswave@users.noreply.github.com>
2022-08-23 13:43:22 -0400

    Update dream.ps1

--- dream.ps1
+++ dream.ps1
@@ -3,6 +3,6 @@ python scripts/stable_txt2img.py --ddim_eta 0.0 `
                                  --n_iter 4 `
                                  --scale 10.0 `
                                  --ddim_steps 50 `
-                                 --embedding_path .\logs\isla_fisher2022-08-23T01-46-37_isla_fisher\checkpoints\embeddings_gs-12199.pt `
+                                 --embedding_path .\logs\my_cat2022-08-23T01-46-37_my_cat\checkpoints\embeddings_gs-12199.pt `
                                  --ckpt ..\stable-diffusion-dream\models\ldm\stable-diffusion-v1\model.ckpt `
-                                 --prompt "magazine photoshoot of * 8k"
+                                 --prompt "photo of * 8k"
